efa864cd597e9b061cd407e81b912309216b7b35
KyryloSitnikov <110498263+KyryloSitnikov@users.noreply.github.com>
2023-05-06 19:14:19 +0300

    111

---
+++ ogneshka.py
@@ -0,0 +1,22 @@
+import telebot
+from datetime import datetime
+
+bot = telebot.TeleBot('6130614504:AAHYFuTbmbNtflJFiUqwsra_vipxSNjRyLA')
+
+responses = {
+    '1': 'My name is Bot',
+    '2': datetime.now().strftime("%H:%M:%S"),
+    '3': 'I am doing well, thanks for asking!'
+}
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.reply_to(message, 'Welcome to my bot!')
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    question = message.text
+    if question in responses:
+        bot.reply_to(message, responses[question])
+
+bot.polling()
