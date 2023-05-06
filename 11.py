d1161a6016d96c25236e1018d701b09ef868368b
KyryloSitnikov <110498263+KyryloSitnikov@users.noreply.github.com>
2023-05-06 20:52:04 +0300

    refactor

--- 11.py
+++
@@ -1,5 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot('6130614504:AAHYFuTbmbNtflJFiUqwsra_vipxSNjRyLA')
-
-print(bot.token)
--- ogneshka.py
+++ ogneshka.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 bot = telebot.TeleBot('6130614504:AAHYFuTbmbNtflJFiUqwsra_vipxSNjRyLA')
 
 responses = {
-    '1': 'My name is Bot',
-    '2': datetime.now().strftime("%H:%M:%S"),
-    '3': 'I am doing well, thanks for asking!'
+    '1': 'Мене звати Огнєшкака, приємно познайомитись)))',
+    '2': 'Зараз' datetime.now().strftime("%H:%M:%S"),
+    '3': 'Все файно), це третя відповідь!'
 }
 print(bot.token)
 @app.route('/' + bot.token, methods=['POST'])
@@ -37,29 +37,3 @@ bot.set_webhook(url='https://ogneshka.herokuapp.com/' + bot.token)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
-
-"""
-import telebot
-from datetime import datetime
-
-bot = telebot.TeleBot('6130614504:AAHYFuTbmbNtflJFiUqwsra_vipxSNjRyLA')
-
-responses = {
-    '1': 'My name is Bot',
-    '2': datetime.now().strftime("%H:%M:%S"),
-    '3': 'I am doing well, thanks for asking!'
-}
-
-@bot.message_handler(commands=['start'])
-def start_command(message):
-    bot.reply_to(message, 'Welcome to my bot!')
-
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    question = message.text
-    if question in responses:
-        bot.reply_to(message, responses[question])
-print('hhh')
-bot.polling()
-"""
