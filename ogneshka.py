414cb0a8b639adf1cc428fc45acaa782791fa9d4
KyryloSitnikov <110498263+KyryloSitnikov@users.noreply.github.com>
2023-05-08 12:36:29 +0300

    nlp

--- ogneshka.py
+++ ogneshka.py
@@ -1,23 +1,13 @@
 import telebot
-from datetime import datetime
+import spacy
+from spacy import displacy
 from flask import Flask, request
 import os
 
 app = Flask(__name__)
 bot = telebot.TeleBot('6130614504:AAHYFuTbmbNtflJFiUqwsra_vipxSNjRyLA')
 
-responses = {
-    '1': 'Мене звати Огнєшкака, приємно познайомитись)))',
-    '2': 'Зараз '+datetime.now().strftime("%H:%M:%S"),
-    '3': 'Все файно), це третя відповідь!'
-}
-
-# Add your allowed user IDs here
-allowed_users = [
-# Kyrylo Sitnikov
-    152274647, 
-# Oleksandr Bondarenko
-    445706100]
+nlp = spacy.load('uk_core_news_lg')
 
 @app.route('/' + bot.token, methods=['POST'])
 def webhook():
@@ -35,14 +25,12 @@ def start_command(message):
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    if message.from_user.id not in allowed_users:
-        bot.reply_to(message, 'Перепрошую, ми з вами не знайомі. Мій батько не дозволяє мені спілкуватися з незнайомими людьми. Бувайте.')
-        return
-    question = message.text
-    if question in responses:
-        bot.reply_to(message, responses[question])
+    text = message.text
+    doc = nlp(text)
+    if doc[0].text.lower() in ('привіт', 'привет'):
+        bot.reply_to(message, 'І вам здоровенькі були!')
     else:
-        bot.reply_to(message, 'Я вас не розумію, перепрошую, задайте інше питання')
+        bot.reply_to(message, 'I do not understand your message.')
 
 bot.remove_webhook()
 bot.set_webhook(url='https://ogneshka.herokuapp.com/' + bot.token)
--- orneshka_1.py
+++ orneshka_1.py
@@ -8,10 +8,17 @@ bot = telebot.TeleBot('6130614504:AAHYFuTbmbNtflJFiUqwsra_vipxSNjRyLA')
 
 responses = {
     '1': 'Мене звати Огнєшкака, приємно познайомитись)))',
-    '2': 'Зараз '+datetime.now().strftime("%H:%M:%S"),
+    '2': 'Зараз '+ datetime.now().strftime("%H:%M:%S"),
     '3': 'Все файно), це третя відповідь!'
 }
-print(bot.token)
+
+# Add your allowed user IDs here
+allowed_users = [
+# Kyrylo Sitnikov
+    152274647, 
+# Oleksandr Bondarenko
+    445706100]
+
 @app.route('/' + bot.token, methods=['POST'])
 def webhook():
     update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
@@ -28,9 +35,14 @@ def start_command(message):
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
+    if message.from_user.id not in allowed_users:
+        bot.reply_to(message, 'Перепрошую, ми з вами не знайомі. Мій батько не дозволяє мені спілкуватися з незнайомими людьми. Бувайте.')
+        return
     question = message.text
     if question in responses:
         bot.reply_to(message, responses[question])
+    else:
+        bot.reply_to(message, 'Я вас не розумію, перепрошую, задайте інше питання')
 
 bot.remove_webhook()
 bot.set_webhook(url='https://ogneshka.herokuapp.com/' + bot.token)
